e45519141e2b288a7000d289247ed39a66dd5d05
Yanda Kiviet <yanda.kiviet@redacademy.co.za>
2026-02-17 14:47:40 +0200

    Change account status to inactive

--- account_status_checker.ts
+++ account_status_checker.ts
@@ -1,6 +1,7 @@
 //We are checking is an acount active or not. The data type being used is boolean.(true or false)
-const isAccountActive: boolean = true;
+const isAccountActive: boolean = false;
 
 if (isAccountActive) 
     {console.log('Your account is active. You may proceed with your transactions.');} 
 else {console.log('Your account is inactive. Please contact our customer support to reactivate your account.')};
+
